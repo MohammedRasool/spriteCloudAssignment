750af3a06ef966f9494b414fe85d33201d79e4dc
Rasool <m.azeez@trengo.com>
2025-05-30 15:04:30 +0200

    configure Cypress with MochAwesome reporter and project settings

---
+++ cypress.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "ygbc93",
+  e2e: {
+    baseUrl: 'https://www.saucedemo.com',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 10000,
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Sauce Demo Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    reportDir: 'cypress/reports'
+  }
+});
